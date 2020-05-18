77c60bac37e7f56d3adb7cee1a9f17b72f8d47e2
Meagle <mindfulyeon@gmail.com>
2020-05-18 19:11:23 +0900

    [스테이트 추가] 클릭시 스크롤 가능하도록 함

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Main.scss";
 
 const Home = () => {
+  const [isScroll, setIsScroll] = useState(false);
+
   return (
-    <div className="home">
+    <div className={`home ${isScroll ? "scroll" : ""}`}>
       <div className="logo">개토톱</div>
       <div className="logo__desc">
         <p>슬개골이 아픈 강아지가 치료받기까지 보호자의 마음고생기</p>
       </div>
-      <div className="bottom__container">
+      <div className="bottom__container" onClick={handleClick}>
         <div className="dog__face">
           <div className="button__text">이야기 듣기</div>
         </div>
       </div>
     </div>
   );
+
+  function handleClick() {
+    setIsScroll(true);
+  }
 };
 
 export default Home;
