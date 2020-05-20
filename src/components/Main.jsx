ba2811670d5c4996144f11ce32fa23607f315eab
Meagle <mindfulyeon@gmail.com>
2020-05-20 21:40:35 +0900

    스크롤 발생하면 translateZ가 변경되어 앞으로 이동하는 기능 추가

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useState, useRef, useCallback } from "react";
+import { useEffect } from "react";
 import "./Main.scss";
 
 const Main = () => {
+  const [scroll, setScroll] = useState(0);
+  const [zmove, setZmove] = useState(0);
+  const [maxScrollValue, setMaxScrollValue] = useState(1);
+
+  const houseRef = useRef(null);
+  const mainRef = useRef(null);
+
+  useEffect(() => {
+    if (mainRef.current) {
+      // const maxScrollValue = mainRef.current.clientHeight - innerHeight;
+      setMaxScrollValue(mainRef.current.clientHeight - window.innerHeight);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [mainRef.current]);
+
+  const handleScroll = useCallback(() => {
+    setScroll(window.pageYOffset);
+    setZmove((scroll / maxScrollValue) * 980 - 490);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [scroll]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [handleScroll]);
+
   return (
-    <div className="main">
+    <div className="main" ref={mainRef}>
       <div className="world">
+        {/* 캐릭터와 하우스를 감싸는 stage */}
         <div className="stage">
-          <div className="house">
+          <div
+            className="house"
+            style={{
+              transform: `translateZ(${zmove}vw)`,
+            }}
+            ref={houseRef}
+          >
             <section className="wall wall-left"></section>
             <section className="wall wall-right"></section>
             <section className="wall wall-front wall-front-a">
