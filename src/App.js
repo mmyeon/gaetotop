9e5aadb77ff319270d7990aa66cefda6aa4149d0
Meagle <mindfulyeon@gmail.com>
2020-05-20 12:46:18 +0900

    [리팩토링] 컴포넌트 이름 수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+// import Intro from "./components/Intro";
 import Main from "./components/Main";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      {/* <Intro /> */}
       <Main />
     </div>
   );
