141f10f39727c5502fc58d1cd1968f8571b27773
Meagle <mindfulyeon@gmail.com>
2020-06-17 18:23:13 +0900

    [리팩토링] 불필요한 코드 삭제

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import Intro from "./containers/Intro/Intro";
 import Main from "./containers/Main/Main";
 import Globalstyle from "./styles/GlobalStyles";
-import FrontDog from "./components/MovingDog/FrontDog";
-
 // TODO: Font옮기고 파일 삭제하기
 import "./App.css";
 
@@ -20,7 +18,6 @@ function App() {
       <Globalstyle />
       {!isVisibleMain && <Intro setIsVisibleMain={setIsVisibleMain} />}
       {isVisibleMain && <Main />}
-      {isVisibleMain && <FrontDog />}
     </div>
   );
 }
